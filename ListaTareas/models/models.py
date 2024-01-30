bb7581035f809188b7411f80059b7cd8a3512979
Zarcam <zarcam.t@gmail.com>
2024-01-30 18:48:43 +0100

    Arreglado: Error de sintaxis en models.py

--- ListaTareas/models/models.py
+++ ListaTareas/models/models.py
@@ -8,7 +8,7 @@ class lista_tareas(models.Model):
     tarea = fields.Char()
     prioridad = fields.Integer()
     urgente = fields.Boolean(compute="_value_urgente", store=True)
-    realizada = fields.Boolean
+    realizada = fields.Boolean()
 
     @api.depends('prioridad')
     def _value_urgente(self):
