b79e024451863643400e464877b56a48e3b297a9
Zarcam <zarcam.t@gmail.com>
2024-01-30 18:07:18 +0100

    Añadido: -> Init -> Manifest

---
+++ ListaTareas/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Lista de tareas",
+
+    'summary': "Sencilla Lista de tareas",
+
+    'description': "Sencilla lista de tareas utilizadas para crear un nuevo modulo con un nuevo modelo de datos",
+
+    'author': "Sergi García"
+    'website': "https://apuntesfpinformatica.es",
+    'application': True,
+
+    'category': 'Productivity',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views,xml',
+    ]
+}
